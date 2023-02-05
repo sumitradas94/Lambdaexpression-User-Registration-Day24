8cff8c3a842b79e23e9fc0fec571d7cc05a57d61
sumitradas94 <sumitradas94@gmail.com>
2023-02-05 20:48:51 +0530

    Greetings

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem using lambda Expression");
+        }
+    }
+}
